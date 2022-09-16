675fdc7b85c60f35735fc40283d1b5c1306a3ab0
Elisey <elisgubarev@gmail.com>
2022-09-16 19:41:07 +0400

    Add button item component

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ButtonItem from "./ButtonItem";
 import "../../../node_modules/figma-plugin-ds/dist/figma-plugin-ds.css";
 import "../styles/style.scss";
 
 const App = () => {
-  return <div className="type">Empty plugin</div>;
+  return <ButtonItem />;
 };
 
 export default App;
---
+++ src/app/components/ButtonItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ButtonItem = (): JSX.Element => {
+  return <div>ButtonItem</div>;
+};
+
+export default ButtonItem;
