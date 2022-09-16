d1e34c005786b22caad50c9d1bac9432b4fdba21
Elisey <elisgubarev@gmail.com>
2022-09-16 19:20:20 +0400

    Formatting

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import '../../../node_modules/figma-plugin-ds/dist/figma-plugin-ds.css';
-import '../styles/style.scss';
+import React from "react";
+import "../../../node_modules/figma-plugin-ds/dist/figma-plugin-ds.css";
+import "../styles/style.scss";
 
 const App = () => {
-    return <div className="type">Empty plugin</div>;
+  return <div className="type">Empty plugin</div>;
 };
 
 export default App;
