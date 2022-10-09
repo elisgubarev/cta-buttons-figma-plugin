2611bab1f522d5975cb6d108830cba43d448ec0c
Elisey <elisgubarev@gmail.com>
2022-10-09 13:13:52 +0400

    Add arrow logic and styles

--- src/app/components/buttons/Classic/Classic.tsx
+++ src/app/components/buttons/Classic/Classic.tsx
@@ -7,16 +7,20 @@ import Arrow from "../../../assets/arrow_classic.svg";
 
 const Classic = (): JSX.Element => {
   const pluginConfig = usePluginConfig();
-  const className = classNames(style.base, {
-    [style.hover]: pluginConfig.hover,
-    [style.outline]: pluginConfig.outline,
+  const { hover, arrow, outline, dark } = pluginConfig;
+
+  const buttonclassNames = classNames(style.base, {
+    [style.hover]: hover,
+    [style.outline]: outline,
   });
+  const arrowClassNames = classNames(style.arrow, {});
+
   return (
-    <button className={className}>
+    <button className={buttonclassNames}>
       <span className={style.text}>
         {buttonTextPropertiesClassic.defaultText}
       </span>
-      <Arrow className={style.arrow} />
+      {arrow && <Arrow className={style.arrow} />}
     </button>
   );
 };
